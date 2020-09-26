75bab5280793a5df95d9dfbac76e7318c63c5f32
mmingst <mmmingst@gmail.com>
2020-09-25 18:11:30 -0700

    fixed lol

--- code/roll.py
+++ code/roll.py
@@ -1,15 +1,16 @@
 import random
+def roll(die):
+    return random.randint(1, die)
 
-def roll(die, adv = False):
-    roll1 = random.randint(0, die)
-    
+def rolladv(adv):
+    roll1 = roll(20)
+    roll2 = roll(20)
+
     if adv == True:
-        roll2 = random.randint(0, die)
-        out = "Rolled a {} and {} for a {}.".format(roll1, roll2, max(roll1, roll2))
+        out = "Rolled a {} and {} with advantage for a {}.".format(roll1, roll2, max(roll1, roll2))
         return out
-
     else:
-        out = "Rolled a {}.".format(roll1)
+        out = "Rolled a {} and {} with disadvantage for a {}.".format(roll1, roll2, min(roll1, roll2))
         return out
 
 def multiroll(q, die, mod = 0):
@@ -19,6 +20,8 @@ def multiroll(q, die, mod = 0):
         d = roll(die)
         totalroll.append(d)
         sum+=d
+    print(totalroll)
+    return sum + mod
     
 
 
@@ -27,15 +30,25 @@ if __name__ == "__main__":
     1. roll a single die
     2. roll a single die with advantage
     3. multiroll a die
+
     """))
     if res == 1:
         d_size = int(input("size?> "))
         print(roll(d_size))
+    
     elif res == 2:
+        adv = int(input("""
+        1. Advantage
+        2. Disadvantage
+
+        """))
+        if adv == 1:
+            print(rolladv(True))
+        else:
+            print(rolladv(False))
+    
+    elif res == 3:
+        d_quant = int(input("How many?> "))       
         d_size = int(input("size?> "))
-        print(roll(d_size, True))
-    # elif res == 3:
-    #     d_quant = int(input("How many?> "))       
-    #     d_size = int(input("size?> "))
-    #     d_mod = int(input("mod?> "))
-    #     print(multiroll(d_quant, d_size, d_mod))
+        d_mod = int(input("mod?> "))
+        print(multiroll(d_quant, d_size, d_mod))
