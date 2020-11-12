8c4a8a953f6d1fd09fead9fbf7c11daee25990d9
fteves22 <60144998+fteves22@users.noreply.github.com>
2020-11-13 05:39:59 +0800

    Update InitTracker.py

--- InitTracker Testing/InitTracker.py
+++ InitTracker Testing/InitTracker.py
@@ -2,6 +2,7 @@ class InitTracker:
     usernames = []
     names = []
     initiatives = []
+    numCombatants = 0
     currentPlayer = 0
     rounds = 0
 
@@ -9,13 +10,14 @@ class InitTracker:
         super().__init__()
 
     def printTracker(self):
-        for i in len(usernames):
+        for i in range(numCombatants):
             print(i + ": " + names[i] + ", " + initiatives[i])
     
     def join(self, username, name, initiative):
         usernames.append(username)
         names.append(name)
         initiatives.append(initiative)
+        numCombatants += 1
     
     def begin(self):
         printTracker()
